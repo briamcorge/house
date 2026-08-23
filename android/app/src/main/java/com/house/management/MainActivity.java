5fb72f54afce6dda231397162552dc5e8369b2fa
Administrator <you@example.com>
2026-08-23 22:28:00 +0800

    导出Excel改用ACTION_VIEW打开: 自定义FileOpener插件替代Share分享,百度网盘等未注册分享接收器的App可在打开菜单中出现,选百度网盘即可保存到网盘; sync 1.252

--- android/app/src/main/java/com/house/management/MainActivity.java
+++ android/app/src/main/java/com/house/management/MainActivity.java
@@ -1,5 +1,12 @@
 package com.house.management;
 
 import com.getcapacitor.BridgeActivity;
+import com.house.management.plugins.FileOpenerPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(android.os.Bundle savedInstanceState) {
+        registerPlugin(FileOpenerPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
