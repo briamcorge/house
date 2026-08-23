1ce0d647bbda16889db04dcd47f4628bdd050476
Administrator <you@example.com>
2026-08-23 22:49:59 +0800

    导出Excel恢复系统分享菜单: 废弃FileOpener插件(华为ROM选择器失效/百度网盘ACTION_VIEW直连失败), 回到Share分享+用户自行保存到网盘; sync 1.257

--- android/app/src/main/java/com/house/management/MainActivity.java
+++ android/app/src/main/java/com/house/management/MainActivity.java
@@ -1,12 +1,5 @@
 package com.house.management;
 
 import com.getcapacitor.BridgeActivity;
-import com.house.management.plugins.FileOpenerPlugin;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(android.os.Bundle savedInstanceState) {
-        registerPlugin(FileOpenerPlugin.class);
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
